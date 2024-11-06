c5f411df6921f5d14b6242f32d07fa53b27a03c7
Pavekatal <pavekatal@gmail.com>
2024-11-07 00:01:10 +0800

    выполнен адаптив блока Мини-игры и подправлен адаптив первого блока для мобильных устройств

--- hw-4.js
+++ hw-4.js
@@ -1,103 +1,103 @@
 // Task 01
 
-// let i = 0;
+let i = 0;
 
-// while (i < 2) {
-//     console.log('Привет!');
-//     i++;
-// }
+while (i < 2) {
+    console.log('Привет!');
+    i++;
+}
 
 // Task 02 
 
-// let a = 1;
+let a = 1;
 
-// while (a <= 5) {
-//     console.log(a);
-//     a++;
-// }
+while (a <= 5) {
+    console.log(a);
+    a++;
+}
 
 // Task 03
 
-// for (c = 7; c <= 22; c++) {
-//     console.log(c);
-// }
+for (c = 7; c <= 22; c++) {
+    console.log(c);
+}
 
 // Task 04 
 
-// const obj = {
-//     Коля: 200,
-//     Вася: 300,
-//     Петя: 400
-// }
+const obj = {
+    Коля: 200,
+    Вася: 300,
+    Петя: 400
+}
 
-// for (let key in obj) {
-//     console.log(`${key} - зарплата ${obj[key]} долларов`);
-// }
+for (let key in obj) {
+    console.log(`${key} - зарплата ${obj[key]} долларов`);
+}
 
 // Task 05
 
-// let n = 1000; 
-// let num = 0; 
+let n = 1000; 
+let num = 0; 
 
-// for (; n >= 50; num++) {
-//     n /= 2;
-// }
+for (; n >= 50; num++) {
+    n /= 2;
+}
 
-// console.log(`Результат делений: ${n}`);
-// console.log(`Количество итераций: ${num}`);
+console.log(`Результат делений: ${n}`);
+console.log(`Количество итераций: ${num}`);
 
 // Task 06 
 
-// let firstFriday = 5; 
+let firstFriday = 5; 
 
-// for (let day = firstFriday; day <= 31; day += 7) {
-//         console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
-// }
+for (let day = firstFriday; day <= 31; day += 7) {
+        console.log(`Сегодня пятница, ${day}-е число. Необходимо подготовить отчет.`);
+}
 
 // Additional task 01
 
-// let k = 100; 
-// let iterations = 0; 
+let k = 100; 
+let iterations = 0; 
 
-// for (; k > 0; iterations++) {
-//     k -= 7; 
-// }
+for (; k > 0; iterations++) {
+    k -= 7; 
+}
 
-// console.log(k);
-// console.log(iterations);
+console.log(k);
+console.log(iterations);
 
 // Additional task 02
 
-// const months = {
-//     1: 'январь',
-//     2: 'февраль',
-//     3: 'март',
-//     4: 'апрель',
-//     5: 'май',
-//     6: 'июнь',
-//     7: 'июль',
-//     8: 'август',
-//     9: 'сентябрь',
-//     10: 'октябрь',
-//     11: 'ноябрь',
-//     12: 'декабрь'
-// };
-// for (let key in months) {
-//    console.log(`месяц ${months[key]} : порядковый номер ${key}`);
-// }
+const months = {
+    1: 'январь',
+    2: 'февраль',
+    3: 'март',
+    4: 'апрель',
+    5: 'май',
+    6: 'июнь',
+    7: 'июль',
+    8: 'август',
+    9: 'сентябрь',
+    10: 'октябрь',
+    11: 'ноябрь',
+    12: 'декабрь'
+};
+for (let key in months) {
+   console.log(`месяц ${months[key]} : порядковый номер ${key}`);
+}
 
 // Additional task 03
 
-// const book = {
-//     name: 'Летос', 
-//     author: 'Алексей Юрьевич Пехов',
-//     yearPublication: 2014,
-//     genre: 'фэнтези'
-// }
+const book = {
+    name: 'Летос', 
+    author: 'Алексей Юрьевич Пехов',
+    yearPublication: 2014,
+    genre: 'фэнтези'
+}
 
-// for (let key in book) {
-//     console.log(`${key} : ${book[key]}`);
-// }
+for (let key in book) {
+    console.log(`${key} : ${book[key]}`);
+}
 
 // Additional task 04
 
