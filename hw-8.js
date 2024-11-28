df3ca1f295bb7d409c41809c891e3780ebefef32
Pavekatal <pavekatal@gmail.com>
2024-11-28 21:06:13 +0800

    выполнены задания по js в файле hw-8.js

---
+++ hw-8.js
@@ -0,0 +1,89 @@
+// Task 01
+
+const peopleArr = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+
+console.log(peopleArr.sort((a, b) => a.age - b.age));
+
+// Task 02
+
+function isPositive(item) {
+    if (item >= 0) {
+        return item;
+    }
+}
+function isMale(item) {
+    if (item.gender === 'male') {
+        return item; 
+    }
+}
+function filter(arr, ruleFunction) {
+    const result = []; 
+
+    for (let i = 0; i < arr.length; i++) {
+        if (ruleFunction(arr[i])) {
+            result.push(ruleFunction(arr[i]));
+        }    
+    }
+
+    return result; 
+}
+    
+console.log(filter([3, -4, 1, 9], isPositive));
+    
+const people = [
+    {name: 'Глеб', gender: 'male'},
+    {name: 'Анна', gender: 'female'},
+    {name: 'Олег', gender: 'male'},
+    {name: 'Оксана', gender: 'female'}
+];
+    
+console.log(filter(people, isMale));
+
+// Task 03 
+
+const today = () => {
+    const currentDate = setInterval(() => {
+        
+        console.log(new Date());
+    }, 3000); 
+
+    setTimeout(() => {
+        clearInterval(currentDate); 
+        console.log('30 секунд прошло');
+    }, 30000);
+};
+
+today(); 
+
+// Task 04
+
+function delayForSecond(callback) {
+    setTimeout(() => {
+        callback();
+    }, 1000); 
+   
+}
+
+delayForSecond(function () {
+   console.log('Привет, Глеб!');
+});
+
+// Task 05
+
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecond(() => sayHi('Глеб'));
