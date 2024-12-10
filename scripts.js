80d4c342b1075d71f52df31da150faad805404a3
Pavekatal <pavekatal@gmail.com>
2024-12-10 20:01:57 +0800

    верстка PixelPerfect мобильной версии сайта, а также добавлены неучтенные ранее эффекты макета

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+document.getElementById('anchor-link').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    let screenWidth = window.innerWidth;
+
+    if (screenWidth <= 768) {
+        document.querySelector('.games__mini').scrollIntoView({ behavior: "smooth" });
+    } else {
+        document.querySelector('.games__about').scrollIntoView({ behavior: "smooth" });
+    }
+
+});
+
 // const runningBox = document.querySelector('.top__nav-box');
 
 // function runningFunc() {
